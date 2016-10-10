9710246efab0bfb1050bffa9f2c3055a97ddbf4b
Olga Stepanova <dragonfly@alternativaplatform.com>
2016-10-11 01:36:03 +0500

    Stones (bitmask)

--- src/Main_1005.java
+++ src/Main_1005.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Куча камней
@@ -19,7 +16,6 @@ public class Main_1005 {
 		int count = Integer.parseInt(reader.readLine());
 
 		int[] stones = StreamUtils.readArray(count, new StreamTokenizer(reader));
-		Arrays.sort(stones);
 
 		int fullSum = 0;
 		for (int i = 0; i < count; i++) {
@@ -28,32 +24,22 @@ public class Main_1005 {
 
 		int minDiff = fullSum;
 
-		for (int size = 1; size <= count; size++) {
-			List<Integer> subsums = subsums(size, stones, 0);
-
-			for (Integer subsum : subsums) {
-				minDiff = Math.min(minDiff, Math.abs(subsum - (fullSum - subsum)));
-			}
+		for (int i = 0; i < 1 << count - 1; i++) {
+			int sum = sum(stones, i);
+			minDiff = Math.min(minDiff, Math.abs(sum - (fullSum - sum)));
 		}
 
 		System.out.println(minDiff);
 	}
 
-	private static List<Integer> subsums(int size, int[] array, int startIndex) {
-		List<Integer> result = new ArrayList<>();
-		if (size <= 0) {
-			return result;
-		}
-		for (int i = startIndex; i <= array.length - size; i++) {
-			if (size == 1) {
-				result.add(array[i]);
-				continue;
-			}
-			List<Integer> subsums = subsums(size - 1, array, i + 1);
-			for (Integer subsum : subsums) {
-				result.add(array[i] + subsum);
+	private static int sum(int[] array, int bitmask) {
+		int sum = 0;
+		for (int i = 0; i < array.length - 1; i++) {
+			if ((bitmask & (1 << i)) != 0) {
+				sum += array[i];
 			}
 		}
-		return result;
+		return sum;
 	}
+
 }
