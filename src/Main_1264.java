1a913be6c0b2269c1b6a3a0cfc229738d9395a6e
Olga Stepanova <olga.s.stepanova@gmail.com>
2016-10-14 01:29:13 +0500

    1264 Трудовые будни

---
+++ src/Main_1264.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Трудовые будни
+ * http://acm.timus.ru/problem.aspx?space=1&num=1264
+ */
+public class Main_1264 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = reader.readLine().split(" ");
+		int n = Integer.parseInt(input[0]);
+		int m = Integer.parseInt(input[1]);
+
+		System.out.println(n * (m + 1));
+	}
+}
