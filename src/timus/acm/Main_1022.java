bfae680863a6d924ee5672447c8713c88ef13974
Olga Stepanova <Olga Stepanova>
2017-04-24 23:44:14 +0500

    1022 Genealogical Tree - Array.sort removed

--- src/timus/acm/Main_1022.java
+++ src/timus/acm/Main_1022.java
@@ -1,13 +1,7 @@
 package timus.acm;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.StreamTokenizer;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 
 public class Main_1022 {
@@ -30,16 +24,19 @@ public class Main_1022 {
 			}
 		}
 
-		int level = 1;
 		int i = 0;
-		while (level <= nodeCount) {
+		int printed = 0;
+		while (printed < nodeCount) {
 			Node node = nodes[i];
-			if (node.parents.isEmpty() && node.level == 0) {
-				node.level = level;
-				level++;
+			if (node != null && node.parents.isEmpty()) {
+				out.print(nodes[i].num);
+				out.print(' ');
+
 				for (Node child : node.children) {
 					child.parents.remove(node);
 				}
+				nodes[i] = null;
+				printed++;
 			}
 			i++;
 			if (i == nodeCount) {
@@ -47,12 +44,6 @@ public class Main_1022 {
 			}
 		}
 
-		Arrays.sort(nodes);
-
-		for (i = 0; i < nodeCount; i++) {
-			out.print(nodes[i].num);
-			out.print(' ');
-		}
 		out.flush();
 	}
 
@@ -61,19 +52,13 @@ public class Main_1022 {
 		return (int) in.nval;
 	}
 
-	private static class Node implements Comparable<Node> {
-		int level;
+	private static class Node {
 		int num;
 		Collection<Node> parents = new ArrayList<>();
 		Collection<Node> children = new ArrayList<>();
 
 		private Node(int num) {
 			this.num = num;
-		}
-
-		@Override
-		public int compareTo(Node o) {
-			return this.level - o.level;
 		}
 
 	}
