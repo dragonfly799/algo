75c6f02f75e7f3a34cd3cb60e9505684d85b8348
Olga Stepanova <olga.s.stepanova@gmail.com>
2017-06-05 22:23:27 +0500

    1073 Square Country: cache in array + iterative version

--- src/timus/acm/Main_1073.java
+++ src/timus/acm/Main_1073.java
@@ -3,28 +3,47 @@ package timus.acm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main_1073 {
-    private static Map<Integer, Integer> cache = new HashMap<>();
+
+    private static int[] cache;
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(in.readLine());
+        cache = new int[n + 1];
 
-        System.out.println(calc(n));
+        System.out.println(calcIter(n));
     }
 
-    private static int calc(int n) {
-        Integer cached = cache.get(n);
-        if (cached != null) {
+    private static int calcIter(int n) {
+        for (int i = 1; i <= n; i++) {
+            int res = i;
+            int sqrt = (int) Math.sqrt(i);
+            if (sqrt * sqrt == i) {
+                res = 1;
+            } else {
+                for (int j = 1; j <= sqrt; j++) {
+                    res = Math.min(res, cache[i - j * j] + 1);
+                    if (res == 2) {
+                        break;
+                    }
+                }
+            }
+            cache[i] = res;
+        }
+        return cache[n];
+    }
+
+    private static int calcRec(int n) {
+        int cached = cache[n];
+        if (cached > 0) {
             return cached;
         }
 
         int sqrt = (int) Math.sqrt(n);
         if (sqrt * sqrt == n) {
-            cache.put(n, 1);
+            cache[n] = 1;
             return 1;
         }
 
@@ -32,7 +51,7 @@ public class Main_1073 {
         int res = n;
 
         for (int i = sqrt; i >= 1; i--) {
-            int calc = calc(n - i * i);
+            int calc = calcRec(n - i * i);
             if (calc < res) {
                 res = calc + 1;
             }
@@ -40,7 +59,7 @@ public class Main_1073 {
                 break;
             }
         }
-        cache.put(n, res);
+        cache[n] = res;
         return res;
     }
 }
