6f1dfee1a12dbe97ff233bc3db56e3f0b25ee1f7
Olga Stepanova <olga.s.stepanova@gmail.com>
2017-07-15 11:23:14 +0500

    1167 Bicolored horses - update

--- src/timus/acm/Main_1167.java
+++ src/timus/acm/Main_1167.java
@@ -7,72 +7,69 @@ import java.io.StreamTokenizer;
 
 public class Main_1167 {
 
-	private static Integer[][] unhappyCache;
-	private static Integer[][] minCache;
+    private static int[][] cache;
 
-	public static void main(String[] args) throws IOException {
-		StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
-		int n = readInt(in);
-		int k = readInt(in);
+    public static void main(String[] args) throws IOException {
+        StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        int n = readInt(in);
+        int k = readInt(in);
 
-		boolean[] horses = new boolean[n];
-		for (int i = 0; i < n; i++) {
-			horses[i] = readInt(in) == 1;
-		}
+        int blackHorses = 0;
+        int[] horses = new int[n];
+        for (int i = 0; i < n; i++) {
+            int horse = readInt(in);
+            blackHorses += horse;
+            horses[i] = blackHorses;
+        }
 
-		minCache = new Integer[n + 1][k + 1];
-		unhappyCache = new Integer[n][n + 1];
+        cache = new int[n][k];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                cache[i][j] = -1;
+            }
+        }
 
-		System.out.println(calcMin(horses, n, k));
-	}
+        System.out.println(calcMin(horses, n, k));
+    }
 
-	private static int calcMin(boolean[] horses, int n, int k) {
-		Integer cached = minCache[n][k];
-		if (cached != null) {
-			return cached;
-		}
+    private static int calcMin(int[] horses, int n, int k) {
+        int cached = cache[n][k];
+        if (cached != -1) {
+            return cached;
+        }
 
-		int min;
-		if (k == 1) {
-			min = calcUnhappy(horses, horses.length - n, horses.length);
-		} else {
-			min = Integer.MAX_VALUE;
-			for (int i = 1; i <= n - k + 1; i++) {
-				int curr = calcUnhappy(horses, horses.length - n, horses.length - n + i) + calcMin(horses, n - i, k - 1);
-				if (curr < min) {
-					min = curr;
-				}
-			}
-		}
-		minCache[n][k] = min;
-		return min;
-	}
+        int min;
+        if (k == 1) {
+            min = calcUnhappy(horses, horses.length - n, horses.length);
+        } else {
+            min = Integer.MAX_VALUE;
+            for (int i = 1; i <= n - k + 1; i++) {
+                int curr = calcUnhappy(horses, horses.length - n, horses.length - n + i) + calcMin(horses, n - i, k - 1);
+                if (curr < min) {
+                    min = curr;
+                }
+            }
+        }
+        cache[n][k] = min;
+        return min;
+    }
 
-	private static int calcUnhappy(boolean[] horses, int startIndex, int endIndex) {
-		if (endIndex - startIndex == 1) {
-			return 0;
-		}
-		Integer cached = unhappyCache[startIndex][endIndex];
-		if (cached != null) {
-			return cached;
-		}
-		int black = 0;
-		int white = 0;
-		for (int i = startIndex; i < endIndex; i++) {
-			if (horses[i]) {
-				black++;
-			} else {
-				white++;
-			}
-		}
-		int res = black * white;
-		unhappyCache[startIndex][endIndex] = res;
-		return res;
-	}
+    private static int calcUnhappy(int[] horses, int startIndex, int endIndex) {
+        if (endIndex - startIndex == 1) {
+            return 0;
+        }
 
-	private static int readInt(StreamTokenizer in) throws IOException {
-		in.nextToken();
-		return (int) in.nval;
-	}
+        int black = horses[endIndex - 1];
+        if (startIndex > 0) {
+            black -= horses[startIndex - 1];
+        }
+        int white = (endIndex - startIndex) - black;
+        return black * white;
+    }
+
+    private static int readInt(StreamTokenizer in) throws IOException {
+        in.nextToken();
+        return (int) in.nval;
+    }
 
 }
